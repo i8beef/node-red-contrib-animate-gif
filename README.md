5fcc6dec18726884b13ff2a4f7de5803873f46ed
Michael Hallock <mike@hallock.net>
2019-10-17 23:21:09 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # node-red-contrib-animate-gif
-Node-red node for combining a set of buffers into an animated gif
+
+Node-red node for combining a set of buffers into an animated gif.
+
+## Getting Started
+
+Install directly from your NodeRED's Setting Pallete
+
+or
+
+This assumes you have [node-red](http://nodered.org/) already installed and working, if you need to install node-red see [here](http://nodered.org/docs/getting-started/installation)
+
+```shell
+$ cd ~/.node-red
+$ npm install node-red-contrib-animate-gif
+```
+## Usage
+
+This package provides a single node, `animate-gif`, which will be under the "functions" group in the pallete. The node exposes configuration settings for delay between frames, image dimensions, quality, and whether to repeat the loop or not. These settings can be set on the node settings, or on the incoming message.
+
+```js
+{
+  delay: 500,
+  dimensionX: 100,
+  dimensionY: 100,
+  quality: 100,
+  repeat: true,
+  payload: [
+      Buffer(...)
+  ]
+}
+```
---
+++ animate-gif.js
@@ -0,0 +1,78 @@
+module.exports = function(RED) {
+    "use strict";
+    const getPixels = require('get-pixels')
+    const GifEncoder = require('gif-encoder');
+
+    function AnimateGifNode(config) {
+        RED.nodes.createNode(this, config);
+
+        // Settings
+        this.delay = config.delay;
+        this.dimensionX = config.dimensionX;
+        this.dimensionY = config.dimensionY;
+        this.quality = config.quality;
+        this.repeat = config.repeat;
+
+        let node = this;
+
+        // Initialize status
+        this.status({ fill: "green", shape: "dot", text: "idle" });
+
+        /*
+         * Global error handler
+         */
+        this.onError = function(error) {
+            node.status({ fill: "red", shape: "dot", text: "error" });
+            node.error(error);
+        };
+
+        /*
+         * Recursive add images to GIF
+         */
+        this.addImageToGif = function(gif, images, counter) {
+            getPixels(images[counter], function(err, pixels) {
+                gif.addFrame(pixels.data);
+                if (counter === images.length - 1) {
+                    gif.finish();
+                } else {
+                    node.addImageToGif(gif, images, ++counter);
+                }
+            });
+        };
+
+        /*
+         * Node-red input handler
+         */
+        this.on("input", function(msg) {
+            // Validate incoming message
+            if (msg.payload == null || !Array.isArray(msg.payload)) {
+                msg.payload = [];
+            }
+
+            msg.delay = msg.delay || node.delay;
+            msg.dimensionX = msg.dimensionX || node.dimensionX;
+            msg.dimensionY = msg.dimensionY || node.dimensionY;
+            msg.quality = msg.quality || node.quality;
+            msg.repeat = msg.repeat || node.repeat;
+
+            try {
+                let gif = new GifEncoder(msg.dimensionX, msg.dimensionY);
+                gif.on('readable', function () {
+                    msg.payload = gif.read();
+                    node.send(msg);
+                });
+
+                gif.setRepeat(msg.repeat)
+                gif.setQuality(msg.quality);
+                gif.setDelay(msg.delay);
+                gif.writeHeader();
+
+                node.addImageToGif(gif, msg.payload.buffers, 0);
+            } catch (exception) {
+                node.onError(exception.message);
+            }
+        });
+    }
+
+    RED.nodes.registerType("animate-gif", AnimateGifNode);
+}
